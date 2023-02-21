afd342b6298499dad0ed89fbc0519f066e4ce5ce
mcarabies <marcelojcarabies@gmail.com>
2023-02-21 11:21:02 -0300

    creando la interfaz del saludo

---
+++ S01_getting_started/s01_06_saludo.py
@@ -0,0 +1,15 @@
+import flet
+from flet import Page, Row, TextField, ElevatedButton, Text
+
+def saludar(event):
+    print("Hola!!!")
+    
+def main(page: Page):
+    row = Row(controls = [
+        TextField(label="Digite su nombre"),
+        ElevatedButton(text="Saludar", on_click=saludar)
+    ])
+
+    page.add(row)
+
+flet.app(target=main)
