0e272cf41a190c8ca450b22464ed3cc96c29165b
mcarabies <marcelojcarabies@gmail.com>
2023-02-21 18:04:06 -0300

    funcion saludar dentro del main

--- S01_getting_started/s01_06_saludo.py
+++ S01_getting_started/s01_06_saludo.py
@@ -1,13 +1,15 @@
 import flet
 from flet import Page, Row, TextField, ElevatedButton, Text
 
-def saludar(event):
-    print("Hola!!!")
-    print("Chau!!!")
+
     
 def main(page: Page):
+    txt_nombre = TextField(label="Digite su nombre")
+    def saludar(event):
+        print(f"Hola {txt_nombre.value}")
+    
     row = Row(controls = [
-        TextField(label="Digite su nombre"),
+        txt_nombre,
         ElevatedButton(text="Saludar", on_click=saludar)
     ])
 
