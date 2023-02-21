371d33a3d108d52c05049cb86245842daa7e9ecb
mcarabies <marcelojcarabies@gmail.com>
2023-02-21 19:05:20 -0300

    app saludar funcionando

--- S01_getting_started/s01_06_saludo.py
+++ S01_getting_started/s01_06_saludo.py
@@ -5,14 +5,22 @@ from flet import Page, Row, TextField, ElevatedButton, Text
     
 def main(page: Page):
     txt_nombre = TextField(label="Digite su nombre")
+    lbl_saludo = Text(value="quieres que te salude?", color="red")
     def saludar(event):
-        print(f"Hola {txt_nombre.value}")
+        lbl_saludo.value=f"hola {txt_nombre.value}"
+        lbl_saludo.color="blue"
+        page.update()
     
-    row = Row(controls = [
+    row1 = Row(controls = [
         txt_nombre,
-        ElevatedButton(text="Saludar", on_click=saludar)
+        ElevatedButton(text="Saludar", on_click=saludar),
     ])
+    row2 =Row(controls=[
+        lbl_saludo
+    ])
+    
+        
 
-    page.add(row)
+    page.add(row1,row2)
 
 flet.app(target=main)
