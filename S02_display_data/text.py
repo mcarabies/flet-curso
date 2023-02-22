78521c8a63d1bdfb902456613ad3d4adb7f7516c
mcarabies <marcelojcarabies@gmail.com>
2023-02-22 17:34:40 -0300

    probando las opciones de display de text

--- S02_display_data/text.py
+++ S02_display_data/text.py
@@ -2,7 +2,16 @@ import flet
 from flet import Page, Text
 
 def main (page:Page):
-    ...
+    lbl_texto = Text(
+        value="Flet y Python",
+        size=30,
+        color="green",
+        bgcolor="black",
+        weight="bold",
+        italic=True
+    )
+    
+    page.add(lbl_texto)
     
 
 flet.app(target=main)
