89a07f7c12a7315f60845f8ee1d54a0069808a50
mcarabies <marcelojcarabies@gmail.com>
2023-02-22 09:32:01 -0300

    campos first name y last name creados

--- S01_getting_started/s01_09_disabled.py
+++ S01_getting_started/s01_09_disabled.py
@@ -1,8 +1,12 @@
 import flet
-from flet import Page, TextField
+from flet import Page, TextField, Column
 
 def main(page:Page):
-    ...
+    txt_first_name = TextField()
+    txt_last_name = TextField()
 
+    col_controles = Column(controls=[txt_first_name,txt_last_name])
+    
+    page.add(col_controles)
 
 flet.app(target=main)
