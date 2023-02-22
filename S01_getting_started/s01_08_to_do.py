316e60577a371e4b1484a4310c274a4ba5d163e0
mcarabies <marcelojcarabies@gmail.com>
2023-02-22 08:49:04 -0300

    app To Do completa

--- S01_getting_started/s01_08_to_do.py
+++ S01_getting_started/s01_08_to_do.py
@@ -1,8 +1,12 @@
 import flet
-from flet import Page, Text
+from flet import Page, Checkbox, ElevatedButton, Row, TextField
 
 def main (page:Page):
-    page.controls.append(Text(value="To Do", color="blue"))
-    page.update()
+    def agregar_tarea_clicked(event):
+        page.add(Checkbox(label=txt_nueva_tarea.value))
+        
+    txt_nueva_tarea = TextField(hint_text="escriba la tarea a agregar", width=300)
+    btn_agregar_tarea = ElevatedButton('Agregar Tarea', on_click=agregar_tarea_clicked)
+    page.add(Row([txt_nueva_tarea, btn_agregar_tarea]))
     
 flet.app(target=main)
