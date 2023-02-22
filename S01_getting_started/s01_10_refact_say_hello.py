594d767f34a2b9f821e20be8e1048157a7e3bfbb
mcarabies <marcelojcarabies@gmail.com>
2023-02-22 15:12:32 -0300

    comenzando la refactorizacion de Say Hello

---
+++ S01_getting_started/s01_10_refact_say_hello.py
@@ -0,0 +1,24 @@
+import flet
+from flet import Page, Column, ElevatedButton, Text, TextField
+
+def main(page:Page):
+    txt_first_name = TextField(label='nombre', autofocus=True)
+    txt_last_name = TextField(label='Apellido')
+    
+    col_controles = Column()
+    
+    def saludar_clicked(event):
+        col_controles.controls.append(Text(f"Hola, {txt_first_name.value} {txt_last_name.value}!!!"))
+        
+        txt_first_name.value = ''
+        txt_last_name.value = ''
+        
+        page.update()
+        
+        txt_first_name.focus()
+    
+    btn_saludar = ElevatedButton('Saludar', on_click=saludar_clicked)
+    
+    page.add(txt_first_name, txt_last_name, btn_saludar, col_controles)
+    
+flet.app(target=main)
