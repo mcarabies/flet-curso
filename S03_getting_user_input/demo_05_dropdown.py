7935e8b5a82711702fb00cf0d20080efe80d065b
mcarabies <marcelojcarabies@gmail.com>
2023-02-24 16:21:00 -0300

    app funcional

--- S03_getting_user_input/demo_05_dropdown.py
+++ S03_getting_user_input/demo_05_dropdown.py
@@ -1,8 +1,24 @@
 import flet
-from flet import Page, Dropdown
+from flet import Page, Dropdown, ElevatedButton, Text, dropdown
 
 def main (page:Page):
-    ...
+    def submit_clicked(event):
+        lbl_resultado.value = f"El valor del Dropdown es {cbx_color.value}"
+        page.update()
+        
+    lbl_resultado = Text()
     
+    btn_submit = ElevatedButton(text="Submit", on_click=submit_clicked)
+    
+    cbx_color = Dropdown(
+        width=300,
+        options= [
+            dropdown.Option('Rojo'),
+            dropdown.Option('Verde'),
+            dropdown.Option('Azul')
+        ]
+    )
+    
+    page.add(cbx_color, btn_submit, lbl_resultado)
     
 flet.app(target=main)
