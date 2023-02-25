7d1ddb0eb4d23de2538ac8717f7a51fba83ad7f7
mcarabies <marcelojcarabies@gmail.com>
2023-02-25 16:39:03 -0300

    eventos de teclado

--- S04_keyboard_shortcuts/demo_01_shortcuts.py
+++ S04_keyboard_shortcuts/demo_01_shortcuts.py
@@ -1,8 +1,15 @@
 import flet
-from flet import Page
+from flet import Page, KeyboardEvent, Text
 
 def main(page:Page):
-    ...
+    def on_keyboard(event:KeyboardEvent):
+        page.add(
+            Text(
+                f'Key: {event.key}, Shift: {event.shift}, Control: {event.ctrl}, Alt: {event.alt}, Meta: {event.meta}'
+            )
+        )
+    page.on_keyboard_event = on_keyboard
+    page.add(Text('Presione cualquier tecla con una combinación (Control, Alt, Shift, Command)'))
     
     
 flet.app(target=main)
