096a0afff192cb1299d483a1342d8d3aaf34e9ee
mcarabies <marcelojcarabies@gmail.com>
2023-02-25 16:55:04 -0300

    agregar 5000 controles

---
+++ S05_large_lists/demo01_5000_text_controls.py
@@ -0,0 +1,11 @@
+import flet
+from flet import Page, Text
+
+def main(page:Page):
+    for i in range(5000):
+        page.controls.append(Text(f"Line: {i}"))
+    
+    page.scroll = "always"
+    page.update()
+    
+flet.app(target=main)
