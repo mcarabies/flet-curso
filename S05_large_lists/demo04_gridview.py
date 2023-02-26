6d3f1440c000589eaca11983ff3cc5a89b6570c3
mcarabies <marcelojcarabies@gmail.com>
2023-02-26 12:42:43 -0300

    gridview teminado

--- S05_large_lists/demo04_gridview.py
+++ S05_large_lists/demo04_gridview.py
@@ -5,7 +5,20 @@ from flet import Page, Container, GridView, Text, alignment, border, border_radi
 os.environ["FLET_WS_MAX_MESSAGE_SIZE"] = "8000000"
 
 def main(page:Page):
-    ...
+    gvw_datos = GridView(expand=True, max_extent=150, child_aspect_ratio=1)
+    page.add(gvw_datos)
+    
+    for i in range(5000):
+        gvw_datos.controls.append(
+            Container(
+                Text(f'item: {i}'),
+                alignment=alignment.center,
+                bgcolor=colors.AMBER_100,
+                border=border.all(1,colors.AMBER_400),
+                border_radius=border_radius.all(5)
+            )
+        )
+    page.update()
     
     
-flet.app(target=main)
+flet.app(target=main, view=flet.WEB_BROWSER)
